90fba2b1d97f3b4261b3b294a5de8dcd4081c347
ConstantMotion1 <constan.fernando@hotmail.com>
2022-09-29 14:44:43 -0400

    updated javascript and html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,48 @@
-<<<<<<< HEAD
-const APIController = (function() {
+let inputEL = document.getElementById('input-el');
+let buttonEL = document.getElementById('btn');
+let artistSearch = inputEL.value.trim();
+// let  = '';
 
-    const clientId = '1d9227c0cc824b66aef7a7edb670448a';
-    const clientSecret = '7e4780e225f643c99b90a189b7e8c46e';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '457cb46fe5msh4f0fca0f45a6a3bp1d9018jsn0107f1e3c983',
+		'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+	}
+};
 
-    //private methods
-    const _getToken = async () => {
 
-        const result = await fetch('https://accounts.spotify.com/api/token', {
-        headers: {
-            'Content-Type' : 'application/x-www-form-urlencoded',
-            'Authorization' : 'Basic' + btoa(clientId + ':' + clientSecret)
-        },
-        body: 'grant_type=client_credentials'
-        });
+fetch('https://deezerdevs-deezer.p.rapidapi.com/artist/402', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
 
-        const data = await result.json();
-        return data.access_token;
-    }
-})();
-=======
-const myList = document.querySelector('ul');
-const myRequest = new Request('products.json');
 
-  $.ajax({
-    type:"GET",
-    url:"https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=92gEVl0gHbeKM91x0LdN2lJHc2OvkU8t",
-    async:true,
-    dataType: "json",
-    success: function(json) {
-                console.log(json);
-                for (const product of data.products) {
-                    const listItem = document.createElement('li');
-                    listItem.appendChild(
-                      document.createElement('strong')
-                    ).textContent = product.Name;
-                    listItem.append(
-                      ` can be found in ${
-                        product.Location
-                      }. Cost: `
-                    );
-                    listItem.appendChild(
-                      document.createElement('strong')
-                    ).textContent = `£${product.Price}`;
-                    myList.appendChild(listItem);
-                // Parse the response.
-                // Do other things.
-             }},
-    error: function(xhr, status, err) {
-                // This time, we do not end up here!
-             }
-  });
->>>>>>> 46309f1b376f40d5518f0c0e493dcab4e5416f4c
+
+
+// function artistCall(event) {
+//     event.preventDefault()
+
+//         fetch('https://deezerdevs-deezer.p.rapidapi.com/search?q=central cee', options)
+//             .then(function (response) {
+//                 return response.json();})
+//             .then(response => console.log(response))
+//             .catch(err => console.error(err));
+//             console.log(inputEL);
+    
+// };
+
+// function fetchCall(artistSearch) {
+//     fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${artistSearch}`, options)
+//         .then(function (response) {
+//             return response.json();})
+//         .then(response => console.log(response))
+//         .catch(err => console.error(err));
+//         console.log(inputEL);
+
+//         };
+
+
+
+
+
